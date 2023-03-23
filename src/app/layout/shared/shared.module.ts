aa7156d05bfdf3908881fd74c072960ab2f41c93
Felipe Basso <fe.wesleybasso@gmail.com>
2023-03-23 00:31:19 -0300

    material module in shared layout module

---
+++ src/app/layout/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+
+import { MaterialModule } from "./material/material.module";
+
+@NgModule({
+    imports: [
+        MaterialModule.forRoot()
+    ],
+    exports: [
+        MaterialModule
+    ]
+})
+export class LayoutSharedModule { }
