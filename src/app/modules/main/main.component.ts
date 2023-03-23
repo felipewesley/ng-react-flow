ec401a29a2d9774dc8cbcd04c9e7146ff2a87f00
Felipe Basso <fe.wesleybasso@gmail.com>
2023-03-23 10:21:38 -0300

    load ng-react-flow lib in main module

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { NotificationService } from "src/app/core/notification/notification.service";
+
+import { ReactFlowEdge, ReactFlowNode } from "src/app/libs/ng-react-flow/react-flow/ReactFlow";
 
 @Component({
     selector: 'app-main',
@@ -7,5 +10,32 @@ import { Component } from "@angular/core";
 })
 export class MainComponent {
 
-    constructor() { }
+    public nodes: ReactFlowNode[] = [];
+    public edges: ReactFlowEdge[] = [];
+  
+    private _initialNodes: ReactFlowNode[] = [
+      { id: '1', position: { x: 0, y: 0 }, data: { label: '1', }, draggable: false },
+      { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+    ];
+    private _initialEdges: ReactFlowEdge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+
+    constructor(
+        private _notificationService: NotificationService
+    ) { }
+
+    public start(): void {
+
+        this.nodes = this._initialNodes;
+        this.edges = this._initialEdges;
+    }
+
+    public onEdgeClicked(event: any): void {
+
+        console.log('click!');
+    }
+
+    public onEdgeAdded(event: any): void {
+
+        this._notificationService.message(`Edge created!`);
+    }
 }
--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,6 +3,8 @@ import { RouterModule } from "@angular/router";
 import { NgReactFlowCommonModule } from "src/app/common/common.module";
 import { LayoutSharedModule } from "src/app/layout/shared/shared.module";
 
+import { NgReactFlowLibModule } from "src/app/libs/ng-react-flow/ng-react-flow.module";
+
 import { mainRoutes as routes } from "./main.routing";
 
 import { MainComponent } from "./main.component";
@@ -11,6 +13,8 @@ import { MainComponent } from "./main.component";
     imports: [
         NgReactFlowCommonModule,
         LayoutSharedModule,
+
+        NgReactFlowLibModule,
 
         RouterModule.forChild(routes)
     ],
