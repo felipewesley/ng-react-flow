d814256050a983be747601b7ade2b41f939f1fa6
Felipe Basso <fe.wesleybasso@gmail.com>
2023-03-23 00:33:33 -0300

    import layout shared module

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { NgReactFlowCommonModule } from "src/app/common/common.module";
+import { LayoutSharedModule } from "src/app/layout/shared/shared.module";
 
 import { mainRoutes as routes } from "./main.routing";
 
@@ -9,6 +10,7 @@ import { MainComponent } from "./main.component";
 @NgModule({
     imports: [
         NgReactFlowCommonModule,
+        LayoutSharedModule,
 
         RouterModule.forChild(routes)
     ],
